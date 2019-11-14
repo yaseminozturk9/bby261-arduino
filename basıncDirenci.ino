0e81f873482ec663421ee15511fd0bf020797be4
Yasemin Öztürk <48058898+yaseminozturk9@users.noreply.github.com>
2019-11-14 11:22:03 +0300

    Create basıncDirenci.ino

---
+++ basıncDirenci.ino
@@ -0,0 +1,67 @@
+const int arduLed1 = 4; //Ardustar Led 1
+const int arduLed2 = 5; //Ardustar Led 2
+const int arduLed3 = 6; //Ardustar Led 3
+const int arduLed4 = 7; //Ardustar Led 4
+/*
+FSR - Force Sensing Resistor
+Kuvvete duyarlı direnç örneği
+*/
+
+int basinc;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Ledlerin bir çıkış olarak tanımlanması
+  pinMode(arduLed1, OUTPUT);
+  pinMode(arduLed2, OUTPUT);
+  pinMode(arduLed3, OUTPUT);
+  pinMode(arduLed4, OUTPUT);
+}
+
+void loop() {
+  basinc = analogRead(0);
+  Serial.println(basinc);
+  
+  
+  if(basinc > 100){
+    digitalWrite(arduLed1, HIGH);
+  }
+  else{
+    digitalWrite(arduLed1, LOW);
+  }
+  
+ if(basinc > 200){
+    digitalWrite(arduLed1, HIGH);
+    digitalWrite(arduLed2, HIGH);
+  }
+  else{
+    digitalWrite(arduLed1, LOW);
+    digitalWrite(arduLed2, LOW);}
+
+  if(basinc > 300){
+    digitalWrite(arduLed1, HIGH);
+    digitalWrite(arduLed2, HIGH);
+    digitalWrite(arduLed3, HIGH);
+  }
+  else{
+    digitalWrite(arduLed1, LOW);
+    digitalWrite(arduLed2, LOW);
+    digitalWrite(arduLed3, LOW);
+  }
+  
+  
+  if(basinc > 400){
+    digitalWrite(arduLed1, HIGH);
+    digitalWrite(arduLed2, HIGH);
+    digitalWrite(arduLed3, HIGH);
+    digitalWrite(arduLed4, HIGH);
+  }
+  else{
+    digitalWrite(arduLed1, LOW);
+    digitalWrite(arduLed2, LOW);
+    digitalWrite(arduLed3, LOW);
+    digitalWrite(arduLed4, LOW);
+  }
+}
+© 2019 GitHub, Inc.
